3dfec5af0543ec626116aaab5d78c09a65cb2759
Your Name <you@example.com>
2023-11-02 08:21:58 +0600

    first header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="text-center">
+            <h2 className="text-3xl font-bold">Course Registration</h2>
+        </div>
+    );
+};
+
+export default Header;
